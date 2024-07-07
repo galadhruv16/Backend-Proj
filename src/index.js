3b2c42c027685c96abc2f2a785dabd929bf538b4
Dhruv Gala <galadhruv1611@gmail.com>
2024-07-08 01:22:57 +0530

    added cors and cookie parser

--- src/index.js
+++ src/index.js
@@ -5,4 +5,12 @@ dotenv.config({
   path: "./env",
 });
 
-connect_db();
+connect_db()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port : ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db connection failed !!", error);
+  });
