941514390af784bb3ab855cbf582bf8f909a6fae
Dhruv Gala <galadhruv1611@gmail.com>
2024-07-09 18:19:26 +0530

    add user and video model

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 const videoSchema = new Schema(
   {
     videoFile: {
@@ -39,3 +39,5 @@ const videoSchema = new Schema(
     timestamps: true,
   }
 );
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
