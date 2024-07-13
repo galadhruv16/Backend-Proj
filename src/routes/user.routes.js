f7b22cafaf2e7bf72674acfd3de3589a63260f68
Dhruv Gala <galadhruv1611@gmail.com>
2024-07-13 21:58:59 +0530

    chore: Add user profile API endpoints and functionality

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controllers.js";
+import {
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+} from "../controllers/user.controllers.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
